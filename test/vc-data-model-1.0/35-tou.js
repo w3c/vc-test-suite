24054050c380ee8f5ab2ca2a699b3ba094a774b2
Manu Sporny <msporny@digitalbazaar.com>
2019-02-10 15:52:58 -0500

    Add optional Terms of Use tests.

---
+++ test/vc-data-model-1.0/35-tou.js
@@ -0,0 +1,26 @@
+/*global describe, it*/
+const config = require('../../config.json');
+const chai = require('chai');
+const {expect} = chai;
+const util = require('./util');
+
+// configure chai
+const should = chai.should();
+chai.use(require('chai-as-promised'));
+
+describe('Terms of Use (optional)', () => {
+  const generatorOptions = {
+    generator: config.generator,
+    args: ""
+  };
+
+  it.skip('MUST support prohibiting Archival', async () => {
+  });
+
+  it.skip('MUST support prohibiting non-subject Presentation', async () => {
+  });
+
+  it.skip('MUST support prohibiting 3rd Party Correlation', async () => {
+  });
+
+});
