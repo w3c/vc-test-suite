1984e11400b5d5db0a7813c7ec8cc8e48be6c045
Brent <brent.zundel@gmail.com>
2019-05-23 14:43:18 -0600

    revert verifiable presentation changes
    Signed-off-by: Brent <brent.zundel@gmail.com>

--- test/vc-data-model-1.0/60-zkp.js
+++ test/vc-data-model-1.0/60-zkp.js
@@ -113,25 +113,10 @@ describe('Zero-Knowledge Proofs (optional)', () => {
 
   });
   describe('A verifiable presentation...', () => {
-    it.skip('MUST be a valid `VerifiablePresentation`', async () => {
-      // test `type` contains `VerifiablePresentation`
-    });
-    it.skip('MUST have a `verifiableCredential` member', async () => {
-      // test `verifiableCredential` exists (and is valid? or is that an additional test?)
-    });
-    describe('Each verifiable credential...', () => {
-      it.skip('MUST have a `credentialSchema` member', async () => {
-      // test that `proof` exists on the embedded credential (and that it's valid?)
-      });
-      it.skip('MUST have a `proof` member', async () => {
-      // test that `proof` exists on the embedded credential (and that it's valid?)
-      });
-    });
-    it.skip('MUST have a `proof` member', async () => {
-    // test that `proof` exists on the embedded credential (and that it's valid?)
-    });
+    // TODO: these 3 tests are "fuzzy"; non-data-model tests--the 3 following have specifics
+    it.skip('All derived verifiable credentials MUST contain a reference to the credential definition used to generate the derived proof.', async () => {
 
-
+    });
     it.skip('All derived proofs in verifiable credentials MUST NOT leak information that would enable the verifier to correlate the holder presenting the credential.', async () => {
 
     });
@@ -142,7 +127,7 @@ describe('Zero-Knowledge Proofs (optional)', () => {
        * leaking personally identifiable information that the holder did not
        * intend to share." */
     });
-    // TODO: these tests MAY be more testable--but may not test the "spirit" of the above requirements...
+    // TODO: these 3 tests MAY be more testable--but may not test the "spirit" of the above requirements...
     it.skip('MUST be a valid `VerifiablePresentation`', async () => {
       // test `type` contains `VerifiablePresentation`
     });
