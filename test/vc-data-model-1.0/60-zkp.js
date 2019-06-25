58aa292befd05905ee6d111f35001f2805b15890
andrewjones <ajones@digitalbazaar.com>
2019-06-25 12:39:13 -0400

    Update to RFC3339.

--- test/vc-data-model-1.0/60-zkp.js
+++ test/vc-data-model-1.0/60-zkp.js
@@ -10,7 +10,6 @@ const { hasType } = util;
 
 // setup constants
 const uriRegex = /\w+:(\/?\/?)[^\s]+/;
-const iso8601Regex = /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$/
 
 // configure chai
 const should = chai.should();
