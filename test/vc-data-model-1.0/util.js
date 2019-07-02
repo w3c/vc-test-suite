3506ced5c954623493da931d7b831c9f14aeb4b6
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-07-02 08:18:07 -0400

    Merge RFC3339 regex changes from PR 65.

--- test/vc-data-model-1.0/util.js
+++ test/vc-data-model-1.0/util.js
@@ -70,8 +70,13 @@ function hasType(doc, expectedType) {
   return type.some(el => el === expectedType);
 }
 
-// https://gist.github.com/marcelotmelo/b67f58a08bee6c2468f8#file-rfc-3339-regex
-const RFC3339regex = /^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))$/
+// RFC3999 regex
+// Z and T can be lowercase
+const RFC3339regex = new RegExp('^(\\d{4})-(0[1-9]|1[0-2])-' +
+  '(0[1-9]|[12][0-9]|3[01])T([01][0-9]|2[0-3]):' +
+  '([0-5][0-9]):([0-5][0-9]|60)' +
+  '(\\.[0-9]+)?(Z|(\\+|-)([01][0-9]|2[0-3]):' +
+  '([0-5][0-9]))$', 'i');
 
 module.exports = {
   generate,
